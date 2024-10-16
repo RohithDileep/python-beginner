18ac014bc7c76a7f4e969dac8dfed27a82fc0249
Rohith Dileep <150414771+RohithDileep@users.noreply.github.com>
2024-10-17 01:51:11 +0800

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,15 @@
+#string
+name = "abhishek"
+print(name)
+#integers
+quantity = 3
+print(f"their are {quantity} football")
+#float
+distance = 5.5
+print(f"i walked {distance} km")
+#boolean
+game=False
+if game:
+    print("we can play today")
+else:
+    print("we cannot play today")
